0617eabcbf543a30d8d64378ce572873cadf60c3
Emmanium <pradoe.ep@gmail.com>
2019-03-06 11:11:46 -0700

    successfully implemented auth0 in LandingPage.js

--- front-end/src/components/LandingPage.js
+++ front-end/src/components/LandingPage.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-export default class LandingPage extends Component {
+class LandingPage extends Component {
   render() {
     return (
       <div>
         <h1>Landing Page</h1>
+        <button onClick={this.props.auth.login}>Sign In</button>
       </div>
     );
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    auth: state.auth
+  };
+};
+
+export default connect(mapStateToProps)(LandingPage);
