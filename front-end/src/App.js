c71572c228ee3ebca2c217be0cf492e33aaaa9ff
Emmanium <pradoe.ep@gmail.com>
2019-03-05 22:22:11 -0700

    re-named SignIn to Callback imported Route and NavLink into App.js added a NavLink and couple Routes into App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+
 import logo from './logo.svg';
 import './styles/App.scss';
 
@@ -6,6 +9,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <NavLink to="/callback" className="nav-button" />
         <h1>The Truth About Your Shape</h1>
         <p>
         A circle is a simple closed shape. It is the set of all points in a plane that are at a given distance from a given point, the centre; equivalently it is the curve traced out by a point that moves so that its distance from a given point is constant. The distance between any of the points and the centre is called the radius. This article is about circles in Euclidean geometry, and, in particular, the Euclidean plane, except where otherwise noted.
@@ -15,6 +19,9 @@ class App extends Component {
           <input type="text">
           </input>
         </form>
+
+        <Route path="/" component={LandingPage} />
+        <Route path="/callback" component={Callback} />
 
       </div>
     );
