3a8deffad5799c7a52f2118beaffef3cf44a2240
Emmanium <pradoe.ep@gmail.com>
2019-03-14 14:27:50 -0600

    refactored Auth0 related code to bring it more up to date added NavBar component

---
+++ front-end/src/components/NavBar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import auth from '../Auth';
+
+function NavBar(props) {
+  const logout = () => {
+    auth.logout();
+    props.history.replace('/');
+  };
+
+  return (
+    <nav className="navbar navbar-dark bg-primary fixed-top">
+      <Link className="navbar-brand" to="/">
+        Q&App
+      </Link>
+      {!auth.isAuthenticated() && (
+        <button className="btn btn-dark" onClick={auth.login}>
+          Sign In
+        </button>
+      )}
+      {auth.isAuthenticated() && (
+        <div>
+          <label className="mr-2 text-white">{auth.getProfile().name}</label>
+          <button
+            className="btn btn-dark"
+            onClick={() => {
+              logout();
+            }}
+          >
+            Sign Out
+          </button>
+        </div>
+      )}
+    </nav>
+  );
+}
+
+export default withRouter(NavBar);
