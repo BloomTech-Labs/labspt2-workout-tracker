33fede1cb69b033966e4e27f2fb77458ca15fa52
Emmanium <pradoe.ep@gmail.com>
2019-03-18 18:27:58 -0600

    Wrapped CheckoutForm in BillingView.js added functionality to the submit method in CheckoutForm.js to tokenize card info sent to server

--- front-end/src/components/BillingView.js
+++ front-end/src/components/BillingView.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import CheckoutForm from './CheckoutForm';
+import { Elements, StripeProvider } from 'react-stripe-elements';
 
-class BillingView extends Component {
+export default class BillingView extends Component {
   render() {
     return (
-      <div>
-        <h1>Billing View</h1>
-      </div>
+      <StripeProvider apiKey="pk_test_TYooMQauvdEDq54NiTphI7jx">
+        <div className="example">
+          <h1>React Stripe Elements</h1>
+          <Elements>
+            <CheckoutForm />
+          </Elements>
+        </div>
+      </StripeProvider>
     );
   }
 }
-
-const mapStateToProps = () => ({});
-
-export default connect(mapStateToProps)(BillingView);
