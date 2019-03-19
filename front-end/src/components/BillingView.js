31174d8953ed8849d29e7f1f5506c5b34e87231c
Emmanium <pradoe.ep@gmail.com>
2019-03-18 21:47:51 -0600

    changed BillingView.js to functional component

--- front-end/src/components/BillingView.js
+++ front-end/src/components/BillingView.js
@@ -2,17 +2,15 @@ import React, { Component } from 'react';
 import CheckoutForm from './CheckoutForm';
 import { Elements, StripeProvider } from 'react-stripe-elements';
 
-export default class BillingView extends Component {
-  render() {
-    return (
-      <StripeProvider apiKey="pk_test_TYooMQauvdEDq54NiTphI7jx">
-        <div className="example">
-          <h1>React Stripe Elements</h1>
-          <Elements>
-            <CheckoutForm />
-          </Elements>
-        </div>
-      </StripeProvider>
-    );
-  }
-}
+const BillingView = () => (
+  <StripeProvider apiKey="pk_test_TYooMQauvdEDq54NiTphI7jx">
+    <div className="example">
+      <h1>React Stripe Elements</h1>
+      <Elements>
+        <CheckoutForm />
+      </Elements>
+    </div>
+  </StripeProvider>
+);
+
+export default BillingView;
