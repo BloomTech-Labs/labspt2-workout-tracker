cbeac9b2dac2664976c3f68e27c30f1c2525392d
Emmanium <pradoe.ep@gmail.com>
2019-03-18 18:30:59 -0600

    added state to Checkoutform to help conditonally render a message when purchase is complete

--- front-end/src/components/CheckoutForm.js
+++ front-end/src/components/CheckoutForm.js
@@ -4,6 +4,9 @@ import { CardElement, injectStripe } from 'react-stripe-elements';
 class CheckoutForm extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      complete: false
+    };
     this.submit = this.submit.bind(this);
   }
 
@@ -20,6 +23,8 @@ class CheckoutForm extends Component {
   }
 
   render() {
+    if (this.state.complete) return <h1>Purchase Complete</h1>;
+
     return (
       <div className="checkout">
         <p>Would you like to complete the purchase?</p>
