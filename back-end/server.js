46c0ed7b792d6f15cf38a0ff94b1b7042a550f71
jbrock1981 <jbrock1981@gmail.com>
2019-03-19 20:56:21 -0500

    individual user endpoint

--- back-end/server.js
+++ back-end/server.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const cors = require("cors");
-const db = require("./data/dbConfig.js");
+const express = require('express');
+const cors = require('cors');
+const db = require('./data/dbConfig.js');
 
 const server = express();
 const corsOptions = {
   credentials: true,
-  origin: "https://workout-tracker-pt2.netlify.com/"
+  origin: 'https://workout-tracker-pt2.netlify.com/'
 };
 
 server.use(express.json());
@@ -19,24 +19,38 @@ function checkForResource(req, res, resource) {
   } else {
     res
       .status(404)
-      .json({ message: "The resource does not exist or is currently empty." });
+      .json({ message: 'The resource does not exist or is currently empty.' });
   }
 }
 
-server.get("/", (req, res) => {
-  res.send({ message: "working so far" });
+server.get('/', (req, res) => {
+  res.send({ message: 'working so far' });
 });
 
-server.get("/api/users", (req, res) => {
-  db("users")
+server.get('/api/users', (req, res) => {
+  db('users')
     .then(users => {
       checkForResource(req, res, users);
     })
     .catch(err => {
-      console.log("error", err);
+      console.log('error', err);
       res
         .status(500)
-        .json({ error: "The users information could not be retrieved." });
+        .json({ error: 'The users information could not be retrieved.' });
+    });
+});
+
+server.get('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+  db('users')
+    .where('id', id)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: 'The specified user could not be retrieved' });
     });
 });
 
