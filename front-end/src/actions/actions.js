9a63585105103ea430f3b446716f94e65161844b
meltzerkyle <meltzerkyle@gmail.com>
2019-03-27 22:27:28 -0500

    configured auth0 frontend

--- front-end/src/actions/actions.js
+++ front-end/src/actions/actions.js
@@ -1,17 +1,22 @@
 import axios from "axios";
+import auth from "../Auth";
 
 export const FETCHED = "FETCHED";
 export const FETCHING = "FETCHING";
 export const FETCHING_ERROR = "FETCHING_ERROR";
 
 export const getUsers = () => {
+  const { getAccessToken } = auth;
+  const headers = { Authorization: `Bearer ${getAccessToken()}` };
   const promise = axios.get(
-    'https://workout-tracker-pt2.herokuapp.com/api/users'
+    "https://workout-tracker-pt2.herokuapp.com/api/users",
+    { headers }
   );
   return dispatch => {
     dispatch({ type: FETCHING });
     promise
       .then(response => {
+        console.log(response);
         dispatch({ type: FETCHED, payload: response.data });
       })
       .catch(err => {
