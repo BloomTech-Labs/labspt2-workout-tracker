4ace5cdc9a9817c8d02edcae4b5b184b44f53fb1
jbrock1981 <jbrock1981@gmail.com>
2019-03-28 20:29:43 -0500

    basic form view

--- front-end/src/components/SettingsView.js
+++ front-end/src/components/SettingsView.js
@@ -2,21 +2,47 @@ import React, { Component } from 'react';
 import './styles/SettingsView.sass';
 
 // import { connect } from 'react-redux';
-import logo from '../images/workout-logo.svg';
+//import logo from '../images/workout-logo.svg';
 class SettingsView extends Component {
   render() {
     return (
       <div className="Main">
-        <img src={logo} />
-        <a>Home</a>
-        <p> ⬣ </p>
-        <h1>Settings</h1>
+        <form>
+          <label>Email:</label>
+          <input placeholder="user@gmail.com" />
+        </form>
+        <form>
+          <label>Phone:</label>
+          <input placeholder="123-123-1234" />
+        </form>
+        <form>
+          <label class="container">
+            Emails?
+            <input type="checkbox" />
+            <span class="checkmark" />
+          </label>
+        </form>
+        <form>
+          <label class="container">
+            Texts?
+            <input type="checkbox" />
+            <span class="checkmark" />
+          </label>
+        </form>
+        <form>
+          <label>Old Password:</label>
+          <input placeholder="**********" />
+        </form>
+        <form>
+          <label>New Password:</label>
+          <input placeholder="**********" />
+        </form>
       </div>
     );
   }
 }
 
-const mapStateToProps = () => ({});
+//const mapStateToProps = () => ({});
 
 // export default connect(mapStateToProps)(SettingsView);
 
