82bd15be1063636e597f75813b16d5bbef7bfcca
Emmanium <pradoe.ep@gmail.com>
2019-04-03 00:06:37 -0600

    added 4 blank endpoints for GET, POST, PUT, and DELETE notes

--- back-end/data/migrations/20190402224221_notes.js
+++ back-end/data/migrations/20190402224221_notes.js
@@ -1,8 +1,20 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('notes', table => {
+    table.increments();
+    //small character limits
+    table.integer('weight').notNullable();
+    table.integer('waist');
+    table.integer('arms');
+    table.integer('notes_id').unsigned();
+
+    // uncomment, save, and knex migrate:latest when ready:
+    // table
+    //  .foreign('user_id);
+    //  .references('id);
+    //  .inTable('users');
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('notes');
 };
