3779fff1c9db21635a1097fccf19d39a93d53e8d
meltzerkyle <meltzerkyle@gmail.com>
2019-04-14 22:15:33 -0500

    completed settings view

--- front-end/src/components/SettingsViewForm.js
+++ front-end/src/components/SettingsViewForm.js
@@ -1,62 +1,112 @@
+import React, { Component } from "react";
+import { updateUser } from "../actions/actions";
+import { connect } from "react-redux";
 
+import "./styles/SettingsViewForm.sass";
 
-import React, { Component } from 'react';
-
-import './styles/SettingsViewForm.sass'
-
-import Checkbox from './Checkbox.jsx'
-
+import Checkbox from "./Checkbox.jsx";
 
 class SettingsViewForm extends Component {
-
-  constructor (props) {
+  constructor(props) {
     super(props);
     this.state = {
-      email: '',
-      phone: '',
-      oldpass: '',
-      newpass: ''
-    }
+      email: "",
+      username: "",
+      newpass: ""
+    };
   }
 
-    inputHandler = (event) => {
-        let value = event.target.value;
-        let property = event.target.dataset.property
+  inputHandler = event => {
+    let value = event.target.value;
+    let property = event.target.dataset.property;
 
-        this.setState({
-          [property]: value
-        })
-    
-        console.log(value)
-    }
+    this.setState({
+      [property]: value
+    });
 
+    console.log(value);
+  };
 
-    render() {
+  submitHandlerEmail = e => {
+    e.preventDefault();
+    const userUpdates = {
+      email: this.state.email
+    };
+    this.props.updateUser(userUpdates);
+  };
 
-      return (
-        <div className='form-container'>
+  submitHandlerUsername = e => {
+    e.preventDefault();
+    const userUpdates = {
+      username: this.state.username
+    };
+    this.props.updateUser(userUpdates);
+  };
+
+  submitHandlerPassword = e => {
+    e.preventDefault();
+    const userUpdates = {
+      password: this.state.password
+    };
+    this.props.updateUser(userUpdates);
+  };
+
+  render() {
+    return (
+      <div className="form-container">
         <form>
           <label>Email:</label>
-          <input onChange={this.inputHandler} type='text' placeholder="email@email.com" value={this.state.email} data-property='email'/>
-          <label>Phone:</label>
-          <input onChange={this.inputHandler} type='text' placeholder="123-123-1234" value={this.state.phone} data-property='phone' />
+          <input
+            onChange={this.inputHandler}
+            type="text"
+            placeholder="email@email.com"
+            value={this.state.email}
+            data-property="email"
+          />
+          <button type="text" onClick={this.submitHandlerEmail}>
+            Save
+          </button>
+          <label>New Username:</label>
+          <input
+            onChange={this.inputHandler}
+            type="text"
+            placeholder="Enter new username"
+            value={this.state.username}
+            data-property="username"
+          />
+          <button type="text" onClick={this.submitHandlerUsername}>
+            Save
+          </button>
           <label className="container">Emails?</label>
           <Checkbox />
           <label className="container">Texts?</label>
           <Checkbox />
-
-          <label>Old Password:</label>
-          <input onChange={this.inputHandler} type='text' placeholder="**********" value={this.state.oldpass} data-property='oldpass' />
           <label>New Password:</label>
-          <input onChange={this.inputHandler} type='text' placeholder="**********" value={this.state.newpass} data-property='newpass'/>
+          <input
+            onChange={this.inputHandler}
+            type="text"
+            placeholder="Enter new password"
+            value={this.state.newpass}
+            data-property="newpass"
+          />
+          <button type="text" onClick={this.submitHandlerPassword}>
+            Save
+          </button>
         </form>
-        </div>
-      );
-    }
+      </div>
+    );
   }
-  
-  //const mapStateToProps = () => ({});
-  
-  // export default connect(mapStateToProps)(SettingsView);
-  
-  export default SettingsViewForm;
+}
+
+const mapStateToProps = state => {
+  console.log("state:", state);
+  return {
+    data: state.data,
+    error: state.error
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { updateUser }
+)(SettingsViewForm);
