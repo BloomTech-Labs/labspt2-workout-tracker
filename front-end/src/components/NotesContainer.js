a9632d44c0e9a9c16b7cde322c23ec6260b81ae4
Phinehas <kohler.nai@gmail.com>
2019-04-17 21:30:12 -0600

    added more styling in NotesContainer.sass added modal to confirm delete for note

--- front-end/src/components/NotesContainer.js
+++ front-end/src/components/NotesContainer.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { defaultNotes } from '../defaults/index';
 import './styles/NotesContainer.sass';
+import Modal from 'react-responsive-modal';
+import axios from 'axios';
 
 class NotesContainer extends Component {
   state = {
-    notes: defaultNotes
+    notes: defaultNotes,
+    open: false
   };
 
   /*   componentDidMount() {
@@ -13,12 +16,38 @@ class NotesContainer extends Component {
     this.setState({ notes: [...this.props.getNotes] });
   } */
 
+  placeholder = () => {
+    return alert('This is a place holder function');
+  };
+
+  onOpenModal = () => {
+    this.setState({ open: !this.state.open });
+  };
+
+  Delete = () => {};
+
   render() {
     return (
       <div className="notes-container">
+        <div />
         {this.state.notes.map(i => {
           return (
-            <div className="note">
+            <div className="note" key={i}>
+              <div>
+                <p onClick={this.placeholder}>Edit</p>
+                <p onClick={this.onOpenModal}>Delete</p>
+                <Modal
+                  styles={{
+                    className: 'note'
+                  }}
+                  open={this.state.open}
+                  onClose={this.onOpenModal}
+                >
+                  <p>Are you sure you would like to delete this note?</p>
+                  <p>Yes</p>
+                  <p onClick={this.onOpenModal}>No</p>
+                </Modal>
+              </div>
               <p>Weight: {i.weight}</p>
               <p>Waist: {i.waist}</p>
               <p>Arms: {i.arms}</p>
