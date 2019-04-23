ecb4d082cee695476061c558a15ab4b4f8c510ac
meltzerkyle <meltzerkyle@gmail.com>
2019-04-22 22:29:48 -0500

    merge conflict resolved

--- front-end/src/components/ScheduleView.js
+++ front-end/src/components/ScheduleView.js
@@ -2,42 +2,29 @@ import auth from "../Auth";
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
-<<<<<<< HEAD
-import Calendar from './Calendar.js'
-import CalendarEvents from './CalendarEvents.js'
-import WorkoutsForm from './WorkoutsForm'
-=======
 import Calendar from "./Calendar.js";
 import CalendarEvents from "./CalendarEvents.js";
->>>>>>> built out user post functionality
+import WorkoutsForm from "./WorkoutsForm";
 
 import "./styles/ScheduleView.sass";
 import "./styles/Calendar.scss";
 
 class ScheduleView extends Component {
   render() {
-    let ScheduledEvents = <CalendarEvents className='events' />
-    if (this.props.dateClicked){
-      ScheduledEvents = <WorkoutsForm />
+    let ScheduledEvents = <CalendarEvents className="events" />;
+    if (this.props.dateClicked) {
+      ScheduledEvents = <WorkoutsForm />;
     }
     return (
-<<<<<<< HEAD
-      <div className='main scheduleView'>
+      <div className="main scheduleView">
         {/* <button onClick={this.props.auth.logout}>Logout</button> */}
         <Calendar />
         {ScheduledEvents}
-=======
-      <div className="main schedule-view">
-        {/* <button onClick={this.props.auth.logout}>Logout</button> */}
-        <Calendar />
-        <CalendarEvents className="events" />
->>>>>>> built out user post functionality
       </div>
     );
   }
 }
 
-<<<<<<< HEAD
 const mapStateToProps = state => {
   return {
     auth: state.auth,
@@ -45,23 +32,7 @@ const mapStateToProps = state => {
     dateClicked: state.dateClicked
   };
 };
-=======
-// const mapStateToProps = state => {
-//   return {
-//     userdata: state.userdata,
-//     error: state.error
-//   };
-// };
-
-// export default connect(
-//   mapStateToProps,
-//   { updateUser }
-// )(ScheduleView);
->>>>>>> built out user post functionality
 
 // export default connect(mapStateToProps)(ScheduleView);
 
-
-export default connect(
-  mapStateToProps
-)(ScheduleView);
+export default connect(mapStateToProps)(ScheduleView);
