88542cc50882429289f2f6f67e217ee5cbf92072
meltzerkyle <meltzerkyle@gmail.com>
2019-05-06 20:19:17 -0500

    updated seed data

--- back-end/data/seeds/03_Exercises.js
+++ back-end/data/seeds/03_Exercises.js
@@ -1,40 +1,96 @@
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex("exercises")
+  return knex('exercises')
     .del()
     .then(function() {
       // Inserts seed entries
-      return knex("exercises").insert([
+      return knex('exercises').insert([
         {
-          exerciseName: "Lowbar Squats",
-          reps: "5",
-          weight: "200 lbs",
-          sets: "5",
+          exerciseName: 'Front Lunges',
+          reps: '12',
+          sets: '1',
+          weight: 'Bodyweight',
           categoryId: 1,
           userId: 1
         },
         {
-          exerciseName: "Lunges",
-          reps: "6",
-          weight: "150 lbs",
-          sets: "4",
-          categoryId: 1,
-          userId: 1
-        },
-        {
-          exerciseName: "Bicep Curls",
-          reps: "5",
-          weight: "100 lbs",
-          sets: "5",
+          exerciseName: 'Bicep Curls with Dumbells',
+          reps: '5-15',
+          sets: '3',
+          weight: '5-10 lb',
           categoryId: 2,
           userId: 1
         },
         {
-          exerciseName: "Preacher Curls",
-          reps: "4",
-          weight: "100 lbs",
-          sets: "4",
+          exerciseName: 'Bicep Curls with Barbells',
+          reps: '5-15',
+          sets: '3',
+          weight: '10 lb',
           categoryId: 2,
+          userId: 1
+        },
+        {
+          exerciseName: 'Dips',
+          reps: '5-12',
+          sets: '3',
+          weight: 'Bodyweight',
+          categoryId: 2,
+          userId: 1
+        },
+        {
+          exerciseName: 'Wall Push-ups',
+          reps: '5-12',
+          sets: '3',
+          weight: 'Bodyweight',
+          categoryId: 2,
+          userId: 1
+        },
+        {
+          exerciseName: 'Push-ups',
+          reps: '5-12',
+          sets: '3',
+          weight: 'Bodyweight',
+          categoryId: 2,
+          userId: 1
+        },
+        {
+          exerciseName: 'Crunches',
+          reps: '10-12',
+          sets: '3',
+          weight: 'Bodyweight',
+          categoryId: 3,
+          userId: 1
+        },
+        {
+          exerciseName: 'Sit-ups',
+          reps: '12-16',
+          sets: '3',
+          weight: 'Bodyweight',
+          categoryId: 3,
+          userId: 1
+        },
+        {
+          exerciseName: 'Jogging',
+          reps: '1',
+          sets: '1',
+          weight: 'Bodyweight',
+          categoryId: 4,
+          userId: 1
+        },
+        {
+          exerciseName: 'Stationary Biking',
+          reps: '1',
+          sets: '1',
+          weight: 'Bodyweight',
+          categoryId: 4,
+          userId: 1
+        },
+        {
+          exerciseName: 'Various',
+          reps: '1',
+          sets: '1',
+          weight: 'Bodyweight',
+          categoryId: 5,
           userId: 1
         }
       ]);
