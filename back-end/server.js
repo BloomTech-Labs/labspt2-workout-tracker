3fad1380f18a772947143cbfcef96ad728bd4217
Christopher Atoki <christopher.atoki@yahoo.com>
2019-05-08 21:46:31 -0400

    Fix deleted braces

--- back-end/server.js
+++ back-end/server.js
@@ -571,6 +571,8 @@ server.get('/api/user/ispremium', checkJwt, (req, res) => {
       res
         .status(500)
         .json({ error: 'User provided either not found or not Premium' });
+    }
+});
 
 // ENDPOINT TO DELETE A NOTE
 
