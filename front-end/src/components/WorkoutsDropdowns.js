7df9047da9337bf4392568d3376324670a319041
Phinehas <kohler.nai@gmail.com>
2019-05-13 22:17:53 -0600

    Added class to the dropdowns in workouts and added some aditional styling for Collapsible component

--- front-end/src/components/WorkoutsDropdowns.js
+++ front-end/src/components/WorkoutsDropdowns.js
@@ -8,33 +8,48 @@ import {
 } from '../actions/actions';
 import { connect } from 'react-redux';
 import Collapsible from 'react-collapsible';
+import './styles/WorkoutsView.sass';
+import './styles/Style.sass';
 
 class WorkoutsDropdowns extends Component {
   state = {};
 
   componentDidMount() {}
 
+  // deleteCategory = () => {
+
+  // };
+
   render() {
     const { categories } = this.props;
     const { exercises } = this.props;
-    console.log('exercises:');
-    console.log(exercises);
+    console.log('categories:');
+    console.log(categories);
     return (
-      <div className='workouts-dropdowns'>
+      <div>
         {categories !== undefined &&
           categories.map((category, i) => {
             return (
-              <Collapsible key={i} trigger={category.categoryName}>
-                {exercises !== undefined &&
-                  exercises.length > 0 &&
-                  exercises
-                    .filter(exercise => {
-                      return exercise.categoryId === category.id;
-                    })
-                    .map((exercise, index) => {
-                      return <p key={index}> {exercise.exerciseName} </p>;
-                    })}
-              </Collapsible>
+              <div className="form-container events-card">
+                <button
+                  onClick={() => {
+                    console.log(this.props.categories[i].id);
+                  }}
+                >
+                  X
+                </button>
+                <Collapsible key={i} trigger={category.categoryName}>
+                  {exercises !== undefined &&
+                    exercises.length > 0 &&
+                    exercises
+                      .filter(exercise => {
+                        return exercise.categoryId === category.id;
+                      })
+                      .map((exercise, index) => {
+                        return <p key={index}> {exercise.exerciseName}</p>;
+                      })}
+                </Collapsible>
+              </div>
             );
           })}
       </div>
