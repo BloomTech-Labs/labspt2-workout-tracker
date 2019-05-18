77ad98bcba6496b77f348da3bc1200303416a078
Emmanium <pradoe.ep@gmail.com>
2019-05-18 10:30:46 -0600

    changed LOCAL back to DEPLOYED removed ProgressNote styling from ProgressView.js

--- front-end/src/containers/ProgressView.js
+++ front-end/src/containers/ProgressView.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ProgressForm from '../components/ProgressForm';
 import ProgressNotes from '../components/ProgressNotes';
 import '../components/styles/ProgressView.sass';
-import '../components/styles/ProgressNotes.sass';
 
 const ProgressView = () => (
   <div className="main progress-view">
